fca7f3b98eb878b374decdf1446b0588c277a024
Cristian-M-B <crisbaronetto@hotmail.com>
2023-10-01 13:01:41 -0300

    add technologies.js

---
+++ src/data/technologies.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        name: 'HTML',
+        image: 'https://icon.icepanel.io/Technology/svg/HTML5.svg'
+    },
+    {
+        name: 'CSS',
+        image: 'https://icon.icepanel.io/Technology/svg/CSS3.svg'
+    },
+    {
+        name: 'Sass',
+        image: 'https://icon.icepanel.io/Technology/svg/Sass.svg'
+    },
+    {
+        name: 'Bootstrap',
+        image: 'https://icon.icepanel.io/Technology/svg/Bootstrap.svg'
+    },
+    {
+        name: 'Tailwind',
+        image: 'https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg'
+    },
+    {
+        name: 'Material UI',
+        image: 'https://icon.icepanel.io/Technology/svg/Material-UI.svg'
+    },
+    {
+        name: 'JavaScript',
+        image: 'https://icon.icepanel.io/Technology/svg/JavaScript.svg'
+    },
+    {
+        name: 'TypeScript',
+        image: 'https://icon.icepanel.io/Technology/svg/TypeScript.svg'
+    },
+    {
+        name: 'React',
+        image: 'https://icon.icepanel.io/Technology/svg/React.svg'
+    },
+    {
+        name: 'Redux',
+        image: 'https://icon.icepanel.io/Technology/svg/Redux.svg'
+    },
+    {
+        name: 'Next',
+        image: 'https://icon.icepanel.io/Technology/svg/Next.js.svg'
+    },
+    {
+        name: 'Socket.IO',
+        image: 'https://icon.icepanel.io/Technology/svg/Socket.IO.svg'
+    },
+    {
+        name: 'Node',
+        image: 'https://icon.icepanel.io/Technology/svg/Node.js.svg'
+    },
+    {
+        name: 'Express',
+        image: 'https://icon.icepanel.io/Technology/svg/Express.svg'
+    },
+    {
+        name: 'Sequelize',
+        image: 'https://icon.icepanel.io/Technology/svg/Sequelize.svg'
+    },
+    {
+        name: 'PostgreSQL',
+        image: 'https://icon.icepanel.io/Technology/svg/PostgresSQL.svg'
+    },
+    {
+        name: 'Mongoose',
+        image: 'https://icon.icepanel.io/Technology/svg/Mongoose.js.svg'
+    },
+    {
+        name: 'MongoDB',
+        image: 'https://icon.icepanel.io/Technology/svg/MongoDB.svg'
+    }
+]
