450ca5f6aac4cf9ddd74a7391f64abda6753e9eb
Cristian-M-B <crisbaronetto@hotmail.com>
2023-10-05 12:20:55 -0300

    add certifications.js

---
+++ src/data/certifications.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        name: 'Full Stack Web Developer',
+        image: '/images/certifications/FullStack.png'
+    },
+    {
+        name: 'Java',
+        image: '/images/certifications/Java.png'
+    },
+    {
+        name: 'TypeScript',
+        image: '/images/certifications/TypeScript.png'
+    },
+    {
+        name: 'Next',
+        image: '/images/certifications/Next.png'
+    },
+    {
+        name: 'Firebase',
+        image: '/images/certifications/Firebase.png'
+    },
+    {
+        name: 'Checkout Pro',
+        image: '/images/certifications/CheckoutPro.png'
+    }
+]
