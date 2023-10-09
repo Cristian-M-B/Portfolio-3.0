331a3b6785d538458b5734908b842cfb83842dd4
Cristian-M-B <crisbaronetto@hotmail.com>
2023-10-09 11:57:07 -0300

    add projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: 'TecnoCommerce',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtH9qFbAw5PfJBjDx8B_r5IKmUP3ig0WM_vQvYxjXu6Be7SA2gRe44yJt74DagxPUXEUM&usqp=CAU',
+        link: 'https://tecnocommerce.vercel.app'
+    },
+    {
+        name: 'Photoland',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQucxOY6USadN6BzMBT5LItbVt-EjPL9f7EU_blXTBYgYpqNpuNs4vttgLYv3oQHZ8cbJw&usqp=CAU',
+        link: 'https://photolandpage.vercel.app'
+    }
+]
