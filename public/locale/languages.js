2f5f6ede2312b0dcac925b5e8e256c7c85749c0f
Cristian-M-B <crisbaronetto@hotmail.com>
2023-10-23 13:12:31 -0300

    add languages.js

---
+++ public/locale/languages.js
@@ -0,0 +1,70 @@
+export default {
+    es: {
+        nav: {
+            home: 'Inicio',
+            technologies: 'Tecnologías',
+            projects: 'Proyectos',
+            certifications: 'Certificaciones',
+            contact: 'Contacto'
+        },
+        titles: {
+            technologies: 'Mis Skills',
+            projects: 'Mis Últimos Proyectos',
+            certifications: 'Mis Certificaciones',
+            contact: 'Contáctame'
+        },
+        home: {
+            title: 'Desarollador Full Stack'
+        },
+        projects: {
+            link: 'Ir al sitio',
+            seeDescription: 'Ver descripción',
+            hideDescription: 'Ocultar descripción',
+            text: [
+                'Tienda de electrodomésticos. La app consta de tres niveles de ingreso (invitado, usuario y administrador). El usuario puede seleccionar productos como favoritos, ver y modificar su perfil y dejar una reseña del producto que compro. Mientras que el administrador puede gestionar los productos, las ordenes de compras y los usuarios registrados. La app incluye pasarela de pago.',
+                'Red social para subir fotos y videos. Los usuarios pueden seguir a otros usuarios, ver sus publicaciones, comentarlas y darles "me gusta". También cuenta con un chat para hablar en tiempo real.'
+            ]
+        },
+        contact: {
+            name: 'Nombre',
+            email: 'Email',
+            subject: 'Asunto',
+            message: 'Mensaje',
+            send: 'Enviar'
+        }
+    },
+    en: {
+        nav: {
+            home: 'Home',
+            technologies: 'Technologies',
+            projects: 'Projects',
+            certifications: 'Certifications',
+            contact: 'Contact'
+        },
+        titles: {
+            technologies: 'My Skills',
+            projects: 'My Latest Projects',
+            certifications: 'My Certifications',
+            contact: 'Contact Me'
+        },
+        home: {
+            title: 'Full Stack Developer'
+        },
+        projects: {
+            link: 'Go to site',
+            seeDescription: 'See description',
+            hideDescription: 'Hide description',
+            text: [
+                'Appliances store. The app consists of three levels of access (guest, user and administrator). The user can select products as favorites, view and modify their profile and leave a review of the product purchased. Meanwhile, the administrator can manage products, purchase orders and registered users. The app includes a payment gateway.',
+                'Social network for uploading photos and videos. Users can follow other users, view their posts, comment on them and like them. It also has a chat to talk in real time.'
+            ]
+        },
+        contact: {
+            name: 'Name',
+            email: 'Email',
+            subject: 'Subject',
+            message: 'Message',
+            send: 'Send'
+        }
+    }
+}
